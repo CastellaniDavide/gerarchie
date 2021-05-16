181a412b3a78905a76fbc04d42b7ae85f03f2a32
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-16 15:12:44 +0200

    Auto-update

--- bin/gerarchie.cpp
+++ bin/gerarchie.cpp
@@ -1,42 +1,67 @@
-/**
- * @file gerarchie.cpp
- *
- * @version 01.01 2021514
- *
- * @brief https://territoriali.olinfo.it/task/gerarchie
- *
- * @ingroup gerarchie
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
- */
-
-// Includes
 #include <bits/stdc++.h>
+#define DEBUG
 using namespace std;
 
-// Variabiles
-int N;
+struct pp {
+    int cultura;
+    int index;
+    vector <pp> seguaci;
+};
 
-// Main code
-int main()
-{
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+int T, N;
+vector <int> R, C;
+queue <pp*> q;
 
-  // Input
-  cin >> N;
+int main() {
+    freopen("input.txt", "r", stdin);
+#ifndef DEBUG
+    freopen("output.txt", "w", stdout);
+#endif // DEBUG
 
-  // Code
-  // ...
+    cin >> T;
 
-  // Output
-  cout << N << endl;
+    for (size_t t = 0; t < T; ++t) {
+        cin >> N;
+        R.resize(N);
+        C.resize(N);
+        pp core;
 
-  // End
-  return 0;
+        for(size_t i = 0; i < N; ++i) {
+            cin >> R[i] >> C[i];
+            if (R[i] == -1) {
+                core.cultura = C[i];
+                core.index = i;
+            }
+        }
+
+        q.push(&core);
+
+        while (!q.empty())
+        {
+            pp* tmp = q.front();
+            q.pop();
+            cout << endl << endl << tmp->index << endl << endl;
+
+            for(size_t i = 0; i < N; ++i) {
+                cout << tmp->index << "\t" << R[i] << endl;
+                if (R[i] == tmp->index)
+                {
+                    cout << "index " << tmp->index << "\t" << i << endl;
+                    cout << "cultura " << tmp->cultura << "\t" << C[i] << endl;
+                    pp persona2;
+                    persona2.cultura = C[i];
+                    persona2.index = i;
+                    cout << "index " << tmp->index << "\t" << persona2.index << endl;
+                    cout << "cultura " << tmp->cultura << "\t" << persona2.cultura << endl;
+
+                    tmp->seguaci.push_back(persona2);
+                    q.push(&persona2);
+                }
+            }
+        }
+        cout << core.seguaci[0].index;
+        cout << core.seguaci[1].index;
+
+        //cout << "Case #" << t + 1 << ": " << sol << endl;
+    }
 }
